0dc2e267e7553affcbd332188166c5330b72c164
fulness4joy <fulness4joy@gmail.com>
2026-07-28 19:49:24 +0300

    DB disable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-echo "Waiting for database..."
+# echo "Waiting for database..."
 
-until mysql -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1
-do
-    sleep 2
-done
+# until mysql -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1
+# do
+#     sleep 2
+# done
 
-echo "Database connected."
+# echo "Database connected."
 
 php artisan key:generate --force
 
