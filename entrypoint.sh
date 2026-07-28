6c1f0ab3324ee06ad2a252901cb3c3f7ea5d7c0e
fulness4joy <fulness4joy@gmail.com>
2026-07-28 20:21:15 +0300

    entrypoint.sh empty test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-php artisan migrate --force
+# php artisan migrate --force
 
-php artisan db:seed --force
+# php artisan db:seed --force
 
-php artisan storage:link || true
+# php artisan storage:link || true
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+# php artisan config:cache
+# php artisan route:cache
+# php artisan view:cache
 
 exec apache2-foreground
