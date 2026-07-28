ca7e342d1dc2d164f03113f8a8ec0535ba190c57
fulness4joy <fulness4joy@gmail.com>
2026-07-29 00:01:16 +0300

    seedrs add log

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,15 +6,30 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
+
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        Log::info('UserSeeder env', [
+            'name' => env('ADMIN_NAME'),
+            'email' => env('ADMIN_EMAIL'),
+        ]);
+
+        Log::info('Users before insert', [
+            'count' => DB::table('users')->count(),
+        ]);
+
         DB::table('users')->insert([
-            'name' => env("ADMIN_NAME"),
-            'email' => env("ADMIN_EMAIL"),
-            'password' => Hash::make(env("ADMIN_PASSWORD")),
+            'name' => env('ADMIN_NAME'),
+            'email' => env('ADMIN_EMAIL'),
+            'password' => Hash::make(env('ADMIN_PASSWORD')),
+        ]);
+
+        Log::info('Users after insert', [
+            'count' => DB::table('users')->count(),
         ]);
     }
 }
