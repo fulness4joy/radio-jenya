52fad9803f401d90ef096c24a0d614d25b0dfde5
fulness4joy <fulness4joy@gmail.com>
2026-07-31 15:46:51 +0300

    Add trust_proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,18 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+    })
+        ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_AWS_ELB,
+        );
+
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
